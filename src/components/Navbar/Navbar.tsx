42a1e3f362e4b20b2a2f70a816dd84c3bbf739a9
alikalatearabi <ali.kalate89@hotmail.com>
2024-12-29 21:50:13 +0330

    change style of navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Button, Layout } from 'antd';
 import { HashLink as Link } from 'react-router-hash-link';
 import { MenuOutlined } from '@ant-design/icons';
+import logo from '../../assets/images/Logo.svg';
 
 import './index.scss';
 
@@ -12,7 +13,15 @@ const Navbar: React.FC = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+    setIsMenuOpen((prev) => {
+      // Toggle the overflow style when the menu is toggled
+      if (!prev) {
+        document.body.style.overflow = 'hidden';
+      } else {
+        document.body.style.overflow = 'auto';
+      }
+      return !prev;
+    });
   };
 
   useEffect(() => {
@@ -25,11 +34,18 @@ const Navbar: React.FC = () => {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
+  useEffect(() => {
+    // Cleanup overflow style when component unmounts or menu is closed
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, []);
+
   return (
     <Layout>
       <Header style={{ zIndex: 1, width: '100%' }} className="navbar">
         <Link to={'/'} className="logo">
-          <p>سالمینا</p>
+        <img src={logo} alt="logo" style={{ width: '45px', marginTop: '25px'}} />
         </Link>
 
         {/* Desktop Menu */}
@@ -62,6 +78,10 @@ const Navbar: React.FC = () => {
             {isMenuOpen && <div className={`overlay ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}></div>}
             <div className={`hamburgerMenu ${isMenuOpen ? 'open' : ''}`}>
               <nav className="mobileMenu">
+                <img src={logo} alt="logo" style={{ width: '80px', marginBottom: '20px' }} />
+                <Button type="primary" href="" download className="headerDownloadButton mobileDownloadButton">
+                  دانلود سالمینا
+                </Button>
                 <ul>
                   <li>
                     <Link smooth to="#keyFeatures" onClick={toggleMenu}>
